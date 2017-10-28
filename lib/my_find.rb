8951d53d986faeb7496969788c8515477c4f9d90
Elle Faraday <faraday.elle@gmail.com>
2017-10-28 21:46:55 +0000

    automatically backed up by learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -4,7 +4,7 @@ def my_find(collection)
   i = 0
   new_collection = Array.new
   while i < collection.length
-    if yield(collection[i]) == true
+    if yield(collection[i])
       return collection[i]
     else
       nil
